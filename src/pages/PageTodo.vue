715c2d48340b88b35ed88469ad23f7dff7b178f3
Massimiliano <massimiliano.porzio@gmail.com>
2021-09-19 22:00:27 +0200

    inizio modulo 6

--- src/pages/PageTodo.vue
+++ src/pages/PageTodo.vue
@@ -1,13 +1,50 @@
 <template>
-  <q-page padding>
-    <p>Todo Page</p>
+  <q-page class="q-pa-md">
+    <q-list bordered>
+      <q-item v-ripple v-for="task in tasks" :key="task.id"
+              clickable
+              @click="task.completed = !task.completed">
+        <q-item-section side top>
+          <q-checkbox v-model="task.completed" />
+        </q-item-section>
+
+        <q-item-section>
+          <q-item-label>{{task.name}}</q-item-label>
+        </q-item-section>
+      </q-item>
+
+
+
+
+    </q-list>
   </q-page>
 </template>
 
 <script>
-import { defineComponent } from 'vue';
+import { defineComponent } from "vue";
 
 export default defineComponent({
-  name: 'PageIndex'
-})
+  name: "PageIndex",
+  data() {
+    return {
+      tasks: [
+        {
+        id: 1,
+        name: 'Go to shop',
+        completed: false
+        },
+        {
+          id: 2,
+          name: 'Get bananas',
+          completed: true
+        },
+        {
+          id: 3,
+          name: 'Get apples',
+          completed: false
+        }
+      ],
+    };
+  },
+});
 </script>
